7c3ff251a6cf1140637ca8f98b73fa8041cf8937
Shital Ganpat Rane <136183661+Shtal-1@users.noreply.github.com>
2023-08-29 15:10:55 +0545

    Update program30.c

--- program30.c
+++ program30.c
@@ -1,20 +1,5 @@
 // Accept number from user and display its factors
 
-// Input :      10
-// Output :    1    2   5
-
-// Input :      9
-// Output :     1     3
-
-// Input :          17
-// Output :         1
-
-// Input :          20
-// Output :         1   2   4   5   10
-
-// Input :          -20
-// Output :         1     2     4       5       10
-
 #include<stdio.h>
 
 void DisplayFactors(int iNo)
@@ -45,32 +30,8 @@ int main()
     return 0;
 }
 
-// Time Complexity : O(N/2)
 
 
-
-/*
-Input : 10
-iNo = 10
-
-if((iNo % 1) == 0)
-{}
-if((iNo % 2) == 0)
-{}
-if((iNo % 3) == 0)
-{}
-if((iNo % 4) == 0)
-{}
-if((iNo % 5) == 0)
-{}
-if((iNo % 6) == 0)
-{}
-if((iNo % 7) == 0)
-{}
-if((iNo % 8) == 0)
-{}
-if((iNo % 9) == 0)
-{}
 
 
 
